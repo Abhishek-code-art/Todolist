bec2a5d012b19c6af71ac72323da9e8c186e4c52
Abhishek <8439tabi@gmail.com>
2022-08-16 19:23:32 +0530

    Add Procfile and update parts

--- app.js
+++ app.js
@@ -153,6 +153,11 @@ app.get("/about", function(req, res) {
 });
 
 // server created
-app.listen(process.env.PORT || 3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
     console.log(`Server is running on ${PORT}`);
 })
